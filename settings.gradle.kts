a42eef35e16ca7a681dcc76a92325ab070b6aa40
unknown <yeasminsabina305@gmail.com>
2023-11-15 17:17:47 +0600

    mec_admin

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "MEC_Admin"
+include(":app")
+ 
